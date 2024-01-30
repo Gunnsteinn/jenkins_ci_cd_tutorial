9b1d08af95d492fd801c751f375cac351d806a9b
Gunnsteinn <guillecolo@gmail.com>
2024-01-30 11:46:46 -0300

    [GC] - Added code.

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-30T11:40:07-0300",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.21 (Ubuntu)"
+    date = "2024-01-30T11:45:07-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_402 (IBM Corporation)"
 )
 @Component
 public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
