cdd50cda9e7c2d3008d868359191fb5b18ef218e
Gunnsteinn <guillecolo@gmail.com>
2024-01-30 21:07:45 -0300

    [GC] - Added front.

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -4,13 +4,13 @@ import com.paymentchain.billing.dto.InvoiceResponse;
 import com.paymentchain.billing.entities.Invoice;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.Generated;
+import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-20T12:52:21+0200",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+    date = "2024-01-30T20:33:48-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.21 (Ubuntu)"
 )
 @Component
 public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
